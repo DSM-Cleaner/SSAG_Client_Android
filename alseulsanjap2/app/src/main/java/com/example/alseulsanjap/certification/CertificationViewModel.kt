17b8ec9cab8aac01c44609e525480ab0435de597
dlswer23 <lbj4298@naver.com>
2022-05-25 19:46:09 +0900

    [UPDATE] certification viewmodel

--- alseulsanjap2/app/src/main/java/com/example/alseulsanjap/certification/CertificationViewModel.kt
+++ alseulsanjap2/app/src/main/java/com/example/alseulsanjap/certification/CertificationViewModel.kt
@@ -19,30 +19,35 @@ class CertificationViewModel : ViewModel() {
     private val _certificationCode: MutableLiveData<CertificationRequest> = MutableLiveData()
     private val _response : MutableLiveData<CertificationResponse> = MutableLiveData()
     private val _successCertification: MutableLiveData<Boolean> = MutableLiveData(false)
+    private val _failCertification : MutableLiveData<Boolean> = MutableLiveData(true)
 
     val certificationCode = _certificationCode
     val response = _response
     val successCertification = _successCertification
-
+    val failCertification = _failCertification
 
     private val _successAddProject = MutableLiveData<Boolean>()
     val successAddProject: LiveData<Boolean> get() = _successAddProject
 
-    val userCode2 = "dssakdjfklsajdf"
     val userCode = MutableLiveData<String>()
     private val _toastMessage = MutableLiveData<String>()
     val toastMessage: LiveData<String> get() = _toastMessage
 
     fun checkCertificationCode() {
         viewModelScope.launch {
-            val response = certificationRespository.doCertification(CertificationRequest(userCode.value!!))
+            val response =
+                certificationRespository.doCertification(CertificationRequest(userCode.value!!))
             if (response.isSuccessful) {
-                _toastMessage.value = "인증에 성공하였습니다."
-                _successCertification.value == true
-                prefs.saveInfo(response.body()!!.authorization, "authorization")
-                prefs.saveInfo(response.body()!!.id, "id")
+                if (response.code() == 200) {
+                    _toastMessage.value = "인증에 성공하였습니다."
 
+                    _successCertification.postValue(true)
+                    prefs.saveInfo(response.body()!!.authorization, "authorization")
+                    prefs.saveInfo(response.body()!!.id, "id")
+                }
             }
+            else
+                _failCertification.value = false
 
         }
     }
--- alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/CertificationActivity.kt
+++ alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/CertificationActivity.kt
@@ -2,6 +2,8 @@ package com.example.alseulsanjap.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.alseulsanjap.BaseActivity
 import com.example.alseulsanjap.R
 import com.example.alseulsanjap.certification.CertificationViewModel
@@ -17,12 +19,19 @@ class CertificationActivity :
         super.onCreate(savedInstanceState)
 
         successCertification()
+        failCertification()
     }
 
     private fun successCertification(){
         if(vm.successCertification.value == true){
-            val intent = Intent(this,CheckResultActivity::class.java)
-            startActivity(intent)
+            Toast.makeText(this,vm.toastMessage.value!!,Toast.LENGTH_SHORT).show()
+            Log.e("asdfasdf","sdfsdfa")
+        }
+    }
+
+    private fun failCertification(){
+        if(vm.failCertification.value == false){
+            binding.textView5.visibility
         }
     }
 }
