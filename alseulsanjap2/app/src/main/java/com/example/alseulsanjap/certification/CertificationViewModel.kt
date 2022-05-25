e5ccb9c1bc9d6a40236ac2daa54bb778b964d465
dlswer23 <lbj4298@naver.com>
2022-05-25 23:30:17 +0900

    [UPDATE] success -> next

--- alseulsanjap2/app/src/main/java/com/example/alseulsanjap/certification/CertificationViewModel.kt
+++ alseulsanjap2/app/src/main/java/com/example/alseulsanjap/certification/CertificationViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.alseulsanjap.certification
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -26,6 +28,9 @@ class CertificationViewModel : ViewModel() {
     val successCertification = _successCertification
     val failCertification = _failCertification
 
+    private val _doneLogin = MutableLiveData(false)
+    val doneLogin: LiveData<Boolean> get() = _doneLogin
+
     private val _successAddProject = MutableLiveData<Boolean>()
     val successAddProject: LiveData<Boolean> get() = _successAddProject
 
@@ -33,6 +38,29 @@ class CertificationViewModel : ViewModel() {
     private val _toastMessage = MutableLiveData<String>()
     val toastMessage: LiveData<String> get() = _toastMessage
 
+//    fun textWatcher() {
+//        editText.addTextChangedListener(object : TextWatcher {
+//            // text watcher methods will be called when the text is changed
+//            override fun beforeTextChanged(
+//                p0: CharSequence?,
+//                p1: Int, p2: Int, p3: Int
+//            ) {
+//                // do something before text changed
+//            }
+//
+//            override fun onTextChanged(
+//                p0: CharSequence?,
+//                p1: Int, p2: Int, p3: Int
+//            ) {
+//                // do something on text changed
+//                textView.text = "On text changed\n\n$p0"
+//            }
+//
+//            override fun afterTextChanged(p0: Editable?) {
+//                // do something after text changed
+//            }
+//        }
+//    }
     fun checkCertificationCode() {
         viewModelScope.launch {
             val response =
@@ -41,7 +69,7 @@ class CertificationViewModel : ViewModel() {
                 if (response.code() == 200) {
                     _toastMessage.value = "인증에 성공하였습니다."
 
-                    _successCertification.postValue(true)
+                    _doneLogin.value = true
                     prefs.saveInfo(response.body()!!.authorization, "authorization")
                     prefs.saveInfo(response.body()!!.id, "id")
                 }
--- alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/CertificationActivity.kt
+++ alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/CertificationActivity.kt
@@ -2,6 +2,7 @@ package com.example.alseulsanjap.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import com.example.alseulsanjap.BaseActivity
@@ -15,6 +16,7 @@ class CertificationActivity :
 
     override val vm: CertificationViewModel by viewModel()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,15 +24,47 @@ class CertificationActivity :
         failCertification()
     }
 
-    private fun successCertification(){
-        if(vm.successCertification.value == true){
-            Toast.makeText(this,vm.toastMessage.value!!,Toast.LENGTH_SHORT).show()
-            Log.e("asdfasdf","sdfsdfa")
+    fun showFragment() {
+        val successFragment = SuccessFragment()
+        //supportFragmentManager.beginTransaction().replace(R.id.certification_container,successFragment)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.certification_container, successFragment)
+        transaction.commit()
+
+        intentActivity()
+
+    }
+
+    private fun intentActivity() {
+        run {
+            Handler().postDelayed({
+                val intent = Intent(this, CheckResultActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            }, DURATION)
         }
     }
 
-    private fun failCertification(){
-        if(vm.failCertification.value == false){
+    companion object {
+        private const val DURATION: Long = 2500
+    }
+
+
+
+
+    private fun successCertification() {
+        vm.doneLogin.observe(this, {
+            if (vm.doneLogin.value == true) {
+                showFragment()
+            }
+        }
+        )
+
+    }
+
+    private fun failCertification() {
+        if (vm.failCertification.value == false) {
             binding.textView5.visibility
         }
     }
