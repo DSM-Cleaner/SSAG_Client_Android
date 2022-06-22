1ed0e6d03f86a80f76f0bcfb67947d5312b6fef5
dlswer23 <lbj4298@naver.com>
2022-06-22 12:05:02 +0900

    [UPDATE] clean code

--- alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/SuccessFragment.kt
+++ alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/SuccessFragment.kt
@@ -9,12 +9,10 @@ import com.example.alseulsanjap.R
 
 class SuccessFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_success, container, false)
     }
 }
