d427d9f9cf89c72ec126dc4055aa86bf8b3da8bb
dlswer23 <lbj4298@naver.com>
2022-06-22 12:05:42 +0900

    [UPDATE] clean code check  result

--- alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/CheckResultActivity.kt
+++ alseulsanjap2/app/src/main/java/com/example/alseulsanjap/ui/CheckResultActivity.kt
@@ -21,9 +21,6 @@ class CheckResultActivity : BaseActivity<ActivityCheckResultBinding>(R.layout.ac
     private val red = context.resources.getColor(R.color.red)
     private val black = context.resources.getColor(R.color.black)
 
-    private val MIN_SCALE = 0.85f // 뷰가 몇퍼센트로 줄어들 것인지
-    private val MIN_ALPHA = 0.5f // 어두워지는 정도를 나타낸 듯 하다
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -73,44 +70,4 @@ class CheckResultActivity : BaseActivity<ActivityCheckResultBinding>(R.layout.ac
             binding.nameNumber = result
         })
     }
-
-
-    inner class ZoomOutPageTransformer : ViewPager2.PageTransformer {
-        override fun transformPage(view: View, position: Float) {
-            view.apply {
-                val pageWidth = width
-                val pageHeight = height
-                when {
-                    position < -1 -> { // [-Infinity,-1)
-                        // This page is way off-screen to the left.
-                        alpha = 0f
-                    }
-                    position <= 1 -> { // [-1,1]
-                        // Modify the default slide transition to shrink the page as well
-                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
-                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
-                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
-                        translationX = if (position < 0) {
-                            horzMargin - vertMargin / 2
-                        } else {
-                            horzMargin + vertMargin / 2
-                        }
-
-                        // Scale the page down (between MIN_SCALE and 1)
-                        scaleX = scaleFactor
-                        scaleY = scaleFactor
-
-                        // Fade the page relative to its size.
-                        alpha = (MIN_ALPHA +
-                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
-                    }
-                    else -> { // (1,+Infinity]
-                        // This page is way off-screen to the right.
-                        alpha = 0f
-                    }
-                }
-            }
-        }
-    }
-
 }
